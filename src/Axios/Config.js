9e403de520314fa4f867a487729f624f104c8280
Jona <alexandercorexp@gmail.com>
2021-02-08 13:37:42 -0600

    1

--- src/Axios/Config.js
+++ src/Axios/Config.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+
+
+const local = "http://localhost:8080";
+const vercel = "https://systemtickets.vercel.app"
 export default axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: vercel
 })
