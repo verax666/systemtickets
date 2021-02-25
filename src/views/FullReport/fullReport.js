7b7577a3ea84b00dd798b02cb7eda5294d567e82
Jona <alexandercorexp@gmail.com>
2021-02-24 18:13:41 -0600

    sds

--- src/views/FullReport/fullReport.js
+++ src/views/FullReport/fullReport.js
@@ -79,7 +79,7 @@ export default function FullReport(props) {
                                                 Comentarios: </Typography>
                                             <Typography variant="h5" className={classes.BodyTitle}>
                                                 {res.ticket.comments} </Typography>
-                                            {console.log(res.tickets.comments)}
+                                            {console.log(res.tickets)}
                                         </Grid>
                                         <Divider />
                                     </Grid>
