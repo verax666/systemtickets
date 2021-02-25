a6b80ded6112573a56026888c5d5b5677d22de53
Jona <alexandercorexp@gmail.com>
2021-02-24 18:26:42 -0600

    v1.8

--- src/services/Methods/methodsTickets.js
+++ src/services/Methods/methodsTickets.js
@@ -41,7 +41,6 @@ export default class MethodsTickets {
     }
 
     async updateTicket(id, status, comments) {
-        console.log(comments)
         await axios.put("/api/ticket/" + id, { statusCatalogId: status, comments: comments }).then(res => { this.Props = { ticket: res.data }; })
 
     }
--- src/views/FullReport/fullReport.js
+++ src/views/FullReport/fullReport.js
@@ -25,6 +25,7 @@ export default function FullReport(props) {
                 )
             ).then(
                 getMethods.getTikcet(props.id).then((res) => {
+                    console.log(res.ticket)
                     setDetails(
                         <>
                             <div className={classes.Body}>
