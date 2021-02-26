2b218ea9a4886ce400d06ec3be817d0914ad6c03
Jona <alexandercorexp@gmail.com>
2021-02-26 11:56:28 -0600

    v1.7

--- src/axios/Config.js
+++ src/axios/Config.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const local = "http://localhost:8080";
 // const heroku = "https://pure-atoll-22967.herokuapp.com"
-const prod = "https://helper.adn-apps.com";
+const prod = "https://tickets.adn-apps.com";
 export default axios.create({
     baseURL: prod
 })
